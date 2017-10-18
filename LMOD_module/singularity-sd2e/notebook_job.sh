70ff10e42c9ac189221db3db1ad78b57a52323ff
Greg Zynda <gzynda@staff.maverick.tacc.utexas.edu>
2017-10-18 11:00:17 -0500

    Updated image name to a static one

--- LMOD_module/singularity-sd2e/notebook_job.sh
+++ LMOD_module/singularity-sd2e/notebook_job.sh
@@ -8,12 +8,12 @@
 #SBATCH -A SD2E-Community
 #SBATCH -t 06:00:00
 
-PASSWORD=`date | md5sum | cut -c-32`
+export PASSWORD=`date | md5sum | cut -c-32`
 RPORT=`shuf -i 2000-65000 -n 1`
 LPORT=8888
 LOGIN=login1.${TACC_SYSTEM}.tacc.utexas.edu
 LOCAL=${HOSTNAME}.${TACC_SYSTEM}.tacc.utexas.edu
-IMG=`ls ${TACC_SD2E_DIR}/sd2e_jupyteruser*img`
+IMG=${TACC_SD2E_DIR}/sd2e_jupyteruser-sd2e_devel.img
 
 ssh -f -g -N -R ${RPORT}:${LOCAL}:${LPORT} ${LOGIN}
 mail -s "SD2E notebook" $EMAIL << EOF
